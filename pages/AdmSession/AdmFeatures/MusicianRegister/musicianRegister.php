424d10e8b94a3e2d645507435dd6ad5ef779f8ba
Giulio Fernandes <giuliofernandes05@gmail.com>
2025-10-02 15:14:13 -0300

    erro de input mask

--- pages/AdmSession/AdmFeatures/MusicianRegister/musicianRegister.php
+++ pages/AdmSession/AdmFeatures/MusicianRegister/musicianRegister.php
@@ -140,11 +140,14 @@
     </div>
   </main>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
   <script>
-    $("#tel").mask("(00) 00000-0000", { placeholder: "(00) 00000-0000" });
-    $("#contactOfResponsible").mask("(00) 00000-0000", { placeholder: "(00) 00000-0000" });
-  </script>  
+    $(document).ready(function(){
+      $("#tel").mask("(00) 00000-0000", { placeholder: "(00) 00000-0000" });
+      $("#contactOfResponsible").mask("(00) 00000-0000", { placeholder: "(00) 00000-0000" });
+    });
+  </script>
+
  </body>
 </html>
