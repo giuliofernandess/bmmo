15f0dfe5325c7c4763175177c920773e86e5a490
Giulio Fernandes <giuliofernandes05@gmail.com>
2025-10-10 10:27:13 -0300

    estilização de musicianEdit

--- pages/AdmSession/AdmFeatures/ListOfMusicians/Profile/MusicianEdit/musicianEdit.php
+++ pages/AdmSession/AdmFeatures/ListOfMusicians/Profile/MusicianEdit/musicianEdit.php
@@ -29,34 +29,64 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Editar Músico</title>
   <script src="../../../../../js/jquery.js"></script>
-  <link rel="shortcut icon" href="images/logo_banda.png" type="image/x-icon" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
+  <link rel="shortcut icon" href="../../../../../../assets/images/logo_banda.png" type="image/x-icon">
+  <link rel="stylesheet" href="../../../../../../assets/css/style.css">
+  <link rel="stylesheet" href="../../../../../../assets/css/form.css">
+  <style>
+    main {
+      padding: 0 30px
+    }
+
+    .login-container {
+      max-width: 1000px;
+    }
+
+    select.form-control, .btn {
+      padding: 0 0 0 12px;
+    }
+  </style>
 </head>
 <body>
 
-  <a href="../musicians.php">Voltar</a>
+  <!-- Header -->
+  <header class="d-flex align-items-center justify-content-between px-3">
+    <a href="#" class="d-flex align-items-center text-white text-decoration-none">
+      <img src="../../../../../../assets/images/logo_banda.png" alt="Logo Banda" width="30" height="30" class="me-2">
+      <span class="fs-5 fw-bold">BMMO Online - Maestro</span>
+    </a>
+    <nav>
+      <ul class="nav">
+        <li class="nav-item">
+          <a href="../musicianProfile.php?idMusician=<?php echo $res['idMusician'] ?>" class="nav-link text-white" style="font-size: 1.4rem;"><i class="bi bi-arrow-90deg-left"></i></a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 
-  <main>
+  <main class="flex-grow-1 d-flex align-items-center justify-content-center flex-column py-5">
     <!-- Formulário -->
-    <div>
-      <form method="post" action="validateMusicianEdit.php">
+    <div class="container login-container">
+      <h1 class="text-center mb-4">Editar Músico</h1>
+      <form method="post" action="validateMusicianEdit.php"  enctype="multipart/form-data" class="row g-3">
 
         <!-- Nome -->
-        <div>
-          <label for="name">Nome</label>
-          <input type="text" name="name" id="name" value="<?php echo $res['name'] ?>" disabled />
+        <div class="col-md-12">
+          <label for="name" class="form-label ps-2">Nome</label>
+          <input type="text" name="name" id="name" value="<?php echo $res['name'] ?>" class="form-control" disabled />
         </div>
 
         <!-- Login -->
-        <div>
-          <label for="login">Login</label>
-          <input type="text" name="login" id="login" value="<?php echo $res['login'] ?>" />
+        <div class="col-md-6">
+          <label for="login" class="form-label ps-2">Login</label>
+          <input type="text" name="login" id="login" value="<?php echo $res['login'] ?>" class="form-control" />
         </div>
 
         <!-- Instrumento -->
-        <div>
-          <label for="instrument">Instrumento</label>
-          <select name="instrument" id="instrument">
+        <div class="col-md-6">
+          <label for="instrument" class="form-label ps-2">Instrumento</label>
+          <select name="instrument" id="instrument" class="form-control">
             <option value="<?php echo $res['instrument'] ?>">Selecione</option>
             <option value="Flauta Doce">Flauta Doce</option>
             <option value="Flute">Flauta</option>
@@ -87,9 +117,9 @@
         </div>
 
         <!-- Grupo da Banda -->
-        <div>
-          <label for="group">Grupo da Banda</label>
-          <select name="bandGroup" id="group">
+        <div class="col-md-6">
+          <label for="group" class="form-label ps-2">Grupo da Banda</label>
+          <select name="bandGroup" id="group" class="form-control">
             <option value="">Selecione</option>
             <option value="Banda Principal">Banda Principal</option>
             <option value="Banda Auxiliar">Banda Auxiliar</option>
@@ -100,27 +130,27 @@
         </div>
 
         <!-- Contato do Músico -->
-        <div>
-          <label for="tel">Contato do Músico</label>
-          <input type="text" name="telephone" id="tel" value="<?php echo $res['telephone'] ?>" />
+        <div class="col-md-6">
+          <label for="tel" class="form-label ps-2">Contato do Músico</label>
+          <input type="text" name="telephone" id="tel" value="<?php echo $res['telephone'] ?>" class="form-control" />
         </div>
 
         <!-- Responsável -->
-        <div>
-          <label for="responsible">Responsável</label>
-          <input type="text" name="responsible" id="responsible" value="<?php echo $res['responsible'] ?>" />
+        <div class="col-md-6">
+          <label for="responsible" class="form-label ps-2">Responsável</label>
+          <input type="text" name="responsible" id="responsible" value="<?php echo $res['responsible'] ?>" class="form-control" />
         </div>
 
         <!-- Contato do Responsável -->
-        <div>
-          <label for="contactOfResponsible">Contato do Responsável</label>
-          <input type="text" name="contactOfResponsible" id="contactOfResponsible" value="<?php echo $res['telephoneOfResponsible'] ?>" />
+        <div class="col-md-6">
+          <label for="contactOfResponsible" class="form-label ps-2">Contato do Responsável</label>
+          <input type="text" name="contactOfResponsible" id="contactOfResponsible" value="<?php echo $res['telephoneOfResponsible'] ?>" class="form-control" />
         </div>
 
         <!-- Bairro -->
-        <div>
-          <label for="neighborhood">Bairro</label>
-          <select name="neighborhood" id="neighborhood">
+        <div class="col-md-6">
+          <label for="neighborhood" class="form-label ps-2">Bairro</label>
+          <select name="neighborhood" id="neighborhood" class="form-control">
             <option value="<?php echo $res['neighborhood'] ?>">Selecione</option>
             <option value="Boa Esperança">Boa Esperança</option>
             <option value="Centro">Centro</option>
@@ -137,37 +167,47 @@
         </div>
 
         <!-- Instituição -->
-        <div>
-          <label for="institution">Instituição</label>
-          <input type="text" name="institution" id="institution" value="<?php echo $res['institution'] ?>" />
+        <div class="col-md-6">
+          <label for="institution" class="form-label ps-2">Instituição</label>
+          <input type="text" name="institution" id="institution" value="<?php echo $res['institution'] ?>" class="form-control" />
         </div>
 
         <!-- Senhas -->
-        <div>
-          <label for="password">Senha</label>
-          <input type="password" name="password" id="password" placeholder="Digite a nova senha" minlength="8" maxlength="20" />
+        <div class="col-md-6">
+          <label for="password" class="form-label ps-2">Senha</label>
+          <input type="password" name="password" id="password" placeholder="Digite a nova senha" minlength="8" maxlength="20" class="form-control" />
         </div>
-        <div>
-          <label for="confirmPassword">Confirmar Senha</label>
-          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirme a nova senha" />
+        <div class="col-md-6">
+          <label for="confirmPassword" class="form-label ps-2">Confirmar Senha</label>
+          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirme a nova senha" class="form-control" />
         </div>
 
         <!-- Botão Editar -->
-        <div>
+        <div class="col-12 mt-3">
           <input type='hidden' name='idMusician' value='<?php echo $res['idMusician'] ?>'>
-          <button type="submit">Editar Músico</button>
+          <button type="submit" class="btn btn-primary btn-lg rounded-pill w-100">Editar Músico</button>
         </div>
       </form>
 
       <!-- Formulário para deletar músico -->
       <form action="MusicianDelete/validateMusicianDelete.php" method="post">
-        <div>
+        <div class="col-12 mt-3">
           <input type='hidden' name='idMusician' value='<?php echo $res['idMusician'] ?>'>
-          <button type="submit">Deletar Músico</button>
+          <button type="submit" class="btn btn-danger btn-lg rounded-pill w-100">Deletar Músico</button>
         </div>
       </form>
     </div>
   </main>
+
+  <!-- Footer -->
+  <footer class="mt-auto py-3">
+    <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+      <span>&copy; Banda de Música</span>
+      <div class="d-flex gap-3">
+        <a href="https://www.instagram.com/bmmooficial" target="_blank"><i class="bi bi-instagram fs-5"></i></a>
+      </div>
+    </div>
+  </footer>
 
   <!-- Scripts -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
