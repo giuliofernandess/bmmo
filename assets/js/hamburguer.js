0aaf556a5e24e57379f34e9852ae2c47f6918f21
giuliofernandess <giuliofernandes05@gmail.com>
2026-02-14 14:02:40 -0300

    Delete hamburguer.js

--- assets/js/hamburguer.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('nav');
-
-  toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active');
-    nav.classList.toggle('active');
-  });
