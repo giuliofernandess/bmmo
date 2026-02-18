59dd942fb35afbf2a769662f21079c8c9f632614
giuliofernandess <giuliofernandes05@gmail.com>
2026-02-18 10:58:38 -0300

    correção de link

--- includes/secondHeader.php
+++ includes/secondHeader.php
@@ -1,6 +1,6 @@
 <!-- Header usado nas features de maestro e músicos -->
 <header class="d-flex align-items-center justify-content-between px-3 mb-auto bg-primary">
-    <a href="#" class="d-flex align-items-center text-white text-decoration-none">
+    <a href="<?= BASE_URL ?>pages/AdmSession/admPage.php" class="d-flex align-items-center text-white text-decoration-none">
       <img src="<?= BASE_URL ?>assets/images/logo_banda.png" alt="Logo Banda" width="30" height="30" class="me-2">
       <span class="fs-5 fw-bold">BMMO Online - Maestro</span>
     </a>
