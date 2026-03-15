39d6d56426db3ae302e02a1224e463df1cf6d9c8
giuliofernandess <giuliofernandes05@gmail.com>
2026-03-15 15:07:32 -0300

    criação da página de perfil do músico

--- pages/MusicianSession/MusicianFeatures/Profile/profile.php
+++ pages/MusicianSession/MusicianFeatures/Profile/profile.php
@@ -1,58 +1,30 @@
 <?php
-require_once '../../../../general-features/bdConnect.php';
+require_once "../../../../config/config.php";
+require_once BASE_PATH . "app/Auth/Auth.php";
+require_once BASE_PATH . "app/Models/Musicians.php";
 
-session_start();
+Auth::requireMusician();
 
-if (!isset($_SESSION['login'])) {
-  echo "<meta http-equiv='refresh' content='0; url=../../../Index/index.php'>";
-}
+$login = $_SESSION["musician_login"] ? trim($_SESSION["musician_login"]) : null;
+$musicianInfo = Musicians::findByLogin($login);
 
-$login = $_SESSION['login'];
-
-if (!$connect) {
-  die("Erro na conexão com o banco de dados.");
-}
-
-$sql = "SELECT * FROM `musicians` WHERE login = '$login'";
-$result = $connect->query($sql);
-
-if (!$result) {
-  die("Erro na consulta: " . $connect->error);
-}
-
-$res = $result->fetch_assoc();
 ?>
 
 <!doctype html>
 <html lang="pt-br">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Meu Perfil</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
-  <link rel="shortcut icon" href="../../../../assets/images/logo_banda.png" type="image/x-icon">
-  <link rel="stylesheet" href="../../../../assets/css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Página do Músico</title>
+
+  <!-- Configurações Básicas -->
+  <?php require_once BASE_PATH . "includes/basicHead.php"; ?>
 </head>
 
 <body>
   <!-- Header -->
-  <header class="d-flex align-items-center justify-content-between px-3">
-    <a href="#" class="d-flex align-items-center text-white text-decoration-none">
-      <img src="../../../../assets/images/logo_banda.png" alt="Logo Banda" width="30" height="30" class="me-2">
-      <span class="fs-5 fw-bold">BMMO Online - Músico</span>
-    </a>
-    <nav>
-      <ul class="nav">
-        <li class="nav-item">
-          <a href="../../musicianPage.php" class="nav-link text-white" style="font-size: 1.4rem;"><i
-              class="bi bi-arrow-90deg-left"></i></a>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <?php include_once BASE_PATH . "includes/secondHeader.php"; ?>
 
   <main class="flex-fill d-flex align-items-center justify-content-center">
     <div class="container">
@@ -60,27 +32,23 @@ $res = $result->fetch_assoc();
         <div class="row g-0">
           <!-- Imagem -->
           <div class="col-md-4 text-center bg-secondary">
-            <img src="../../../../assets/images/musicians-images/<?php if ($res['image'] == "") {
-              echo 'default.png';
-            } else {
-              echo $res['image'];
-            } ?>" class="img-fluid rounded-start h-100 object-fit-cover"
-              alt="Imagem de <?php echo htmlspecialchars($res['name']) ?>">
+            <img src="<?= BASE_URL ?>uploads/musicians-images/<?= $musicianInfo['profile_image'] ? $musicianInfo['profile_image'] : 'default.png'?>" class="img-fluid rounded-start h-100 object-fit-cover"
+              alt="Imagem de <?= htmlspecialchars($musicianInfo['musician_name']) ?>">
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h4 class="card-title mb-3"><?php echo htmlspecialchars($res['name']); ?></h4>
+              <h4 class="card-title mb-3"><?= htmlspecialchars($musicianInfo['musician_name']); ?></h4>
               <ul class="list-group list-group-flush">
                 <li class="list-group-item"><strong>Instrumento:</strong>
-                  <?php echo htmlspecialchars($res['instrument']); ?></li>
-                <li class="list-group-item"><strong>Grupo:</strong> <?php echo htmlspecialchars($res['bandGroup']); ?>
+                  <?= htmlspecialchars($musicianInfo['instrument_name']); ?></li>
+                <li class="list-group-item"><strong>Grupo:</strong> <?= htmlspecialchars($musicianInfo['group_name']); ?>
                 </li>
                 <li class="list-group-item"><strong>Data de Nascimento:</strong>
-                  <?php echo htmlspecialchars($res['dateOfBirth']); ?></li>
+                  <?= htmlspecialchars($musicianInfo['date_of_birth']); ?></li>
                 <li class="list-group-item"><strong>Telefone:</strong>
-                  <?php echo htmlspecialchars($res['telephone']); ?></li>
+                  <?= htmlspecialchars($musicianInfo['musician_contact']); ?></li>
                 <li class="list-group-item"><strong>Bairro:</strong>
-                  <?php echo htmlspecialchars($res['neighborhood']); ?></li>
+                  <?= htmlspecialchars($musicianInfo['neighborhood']); ?></li>
               </ul>
             </div>
           </div>
@@ -90,7 +58,7 @@ $res = $result->fetch_assoc();
   </main>
 
   <!-- Footer -->
-  <?php require_once '../../../../general-features/footer.php'; ?>
+  <?php include_once BASE_PATH . "includes/footer.php"; ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
