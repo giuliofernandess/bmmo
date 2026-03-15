f4471aca0a8c6f564a176342fb2265e8e7b130a7
giuliofernandess <giuliofernandes05@gmail.com>
2026-03-15 14:40:28 -0300

    criação da página do músico

--- pages/MusicianSession/musicianPage.php
+++ pages/MusicianSession/musicianPage.php
@@ -1,19 +1,13 @@
 <?php
-session_start();
+require_once "../../config/config.php";
+require_once BASE_PATH . "app/Auth/Auth.php";
+require_once BASE_PATH . "app/Models/Musicians.php";
 
-if (!isset($_SESSION['login'])) {
-  echo "<meta http-equiv='refresh' content='0; url=../Index/index.php'>";
-}
+Auth::requireMusician();
 
-require_once "../../general-features/bdConnect.php";
+$login = $_SESSION["musician_login"] ? trim($_SESSION["musician_login"]) : null;
+$musicianInfo = Musicians::findByLogin($login);
 
-$login = $_SESSION["login"];
-
-$stmt = $connect->prepare("SELECT name FROM `musicians` WHERE login = ?");
-$stmt->bind_param("s", $login);
-$stmt->execute();
-$result = $stmt->get_result();
-$res = $result->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -24,27 +18,28 @@ $res = $result->fetch_assoc();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Página do Músico</title>
 
-  <!-- Links -->
-  <link rel="shortcut icon" href="../../assets/images/logo_banda.png" type="image/x-icon">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-  <link rel="stylesheet" href="../../assets/css/style.css">
+  <!-- Configurações Básicas -->
+  <?php require_once BASE_PATH . "includes/basicHead.php"; ?>
 </head>
 
 <body>
+  <!-- Toast de sucesso -->
+  <?php include_once BASE_PATH ."includes/successToast.php"; ?>
+
   <!-- Header -->
-  <header class="d-flex align-items-center justify-content-between px-3 mb-auto">
+  <header class="d-flex align-items-center justify-content-between px-3 bg-primary">
     <a href="#" class="d-flex align-items-center text-white text-decoration-none">
-      <img src="../../assets/images/logo_banda.png" alt="Logo Banda" width="30" height="30" class="me-2">
+      <img src="<?= BASE_URL ?>assets/images/logo_banda.png" alt="Logo Banda" width="30" height="30" class="me-2">
       <span class="fs-5 fw-bold">BMMO Online - Músico</span>
     </a>
 
     <nav>
       <ul class="nav">
         <li class="nav-item">
-          <a href="../../general-features/logout.php" class="nav-link text-white"
-            onclick="return confirm('Tem certeza que deseja sair de sua conta?');" style="font-size: 1.6rem;"><i
-              class="bi bi-box-arrow-right fs-2 text-white mb-2"></i></a>
+          <a href="<?= BASE_URL ?>pages/logout.php" class="nav-link text-white"
+            onclick="return confirm('Tem certeza que deseja sair de sua conta?');" style="font-size: 1.6rem;">
+            <i class="bi bi-box-arrow-left text-white"></i>
+          </a>
         </li>
       </ul>
     </nav>
@@ -53,8 +48,8 @@ $res = $result->fetch_assoc();
   <!-- Main -->
   <main class="flex-grow-1 d-flex align-items-center justify-content-center py-5">
     <div class="container introduction-container text-center">
-      <img src="../../assets/images/logo_banda.png" width="160" height="160" alt="Logo" class="mb-3">
-      <h1 class="fw-bold">Bem-vindo, <?php echo htmlspecialchars($res['name']); ?></h1>
+      <img src="<?= BASE_URL ?>assets/images/logo_banda.png" width="160" height="160" alt="Logo" class="mb-3">
+      <h1 class="fw-bold">Bem-vindo(a), <?= htmlspecialchars($musicianInfo['musician_name']); ?></h1>
       <p class="mb-4">
         Aqui você pode acessar suas informações, ensaios, partituras e muito mais.
       </p>
@@ -73,10 +68,10 @@ $res = $result->fetch_assoc();
 
         <!-- Card 2 -->
         <div class="col">
-          <a href="MusicianFeatures/Repertoire/musicianRepertoire.php" class="text-decoration-none text-dark">
+          <a href="MusicianFeatures/Presentations/musicianPresentations.php" class="text-decoration-none text-dark">
             <div class="card h-100 shadow-sm border-0 p-3">
               <i class="bi bi-calendar-event-fill fs-2 text-success mb-2"></i>
-              <h5 class="fw-bold">Repertório</h5>
+              <h5 class="fw-bold">Apresentações</h5>
               <p class="text-muted">Veja as músicas das próximas tocatas.</p>
             </div>
           </a>
@@ -84,7 +79,7 @@ $res = $result->fetch_assoc();
 
         <!-- Card 3 -->
         <div class="col">
-          <a href="MusicianFeatures/MusicalScores/musicianMusicalScores.php" class="text-decoration-none text-dark">
+          <a href="MusicianFeatures/MusicalScores/musicalScores.php" class="text-decoration-none text-dark">
             <div class="card h-100 shadow-sm border-0 p-3">
               <i class="bi bi-music-note-beamed fs-2 text-warning mb-2"></i>
               <h5 class="fw-bold">Partituras</h5>
@@ -97,7 +92,7 @@ $res = $result->fetch_assoc();
   </main>
 
   <!-- Footer -->
-  <?php require_once '../../general-features/footer.php'; ?>
+  <?php include_once BASE_PATH . 'includes/footer.php'; ?>
 
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
